1452726946f4ccc5602e8d1d2bfc81e9555cf20a
Volodymyr Bakhmatiuk <volodymyr.bakhmatiuk@rokittech.com>
2017-03-22 15:34:12 +0200

    Initial commit

---
+++ src/main/java/demo/Client.java
@@ -0,0 +1,20 @@
+package demo;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by volodymyr.bakhmatiuk on 3/9/17.
+ */
+public class Client {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ClientConfig config = new ClientConfig();
+        config.getNetworkConfig().setAddresses(
+                Arrays.asList("localhost:5071", "localhost:5072", "localhost:5073")
+        );
+        HazelcastClient.newHazelcastClient(config);
+    }
+}
---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,26 @@
+package demo;
+
+import com.hazelcast.config.FileSystemXmlConfig;
+import com.hazelcast.core.Hazelcast;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+/**
+ * Created by volodymyr.bakhmatiuk on 3/9/17.
+ */
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Arrays.asList(
+                "hazelcast-config-5072.xml",
+                "hazelcast-config-5073.xml"
+        )
+                .forEach(configName -> {
+                    try {
+                        Hazelcast.newHazelcastInstance(new FileSystemXmlConfig(configName));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
